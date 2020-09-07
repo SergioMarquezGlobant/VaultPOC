751f443106793ec4da35801cb5430d610469c8ea
Sergio Efrain Marquez Mora <sergio.marquez@globant.com>
2020-09-07 11:31:53 -0500

    fix console

---
+++ VaultConsolePOC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using VaultCore;
+
+namespace VaultConsolePOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VaultOperations vaultOperations = new VaultOperations();
+            var secrets =  vaultOperations.GetKVSecrets("dotnet").Result;
+
+            Console.WriteLine(secrets.ToList());
+        }
+    }
+}
